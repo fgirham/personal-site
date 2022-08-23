d8d351942f680994f216213ed9624655a402fe1e
fgirham <fghaniirham1@gmail.com>
2022-08-23 18:23:48 +0700

    feat: remove currently unused navigation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,18 +1,42 @@
-import logo from '../../assets/logo-fgi.svg'
-import styles from './styles.module.css'
+import { Link, NavLink, Navigate, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo-fgi.svg";
+import styles from "./Navbar.module.css";
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <nav>
       <div className={styles["navbar-brand"]}>
-        <img className={styles["logo"]} src={logo} alt=""/>
+        <Link to="/">
+          <img className={styles["logo"]} src={logo} alt="" />
+        </Link>
       </div>
       <div className={styles["nav-link"]}>
-        <div className={styles["nav-item"]}>About</div>
-        <div className={styles["nav-item"]}>Experience</div>
+        <Link to="/">Home</Link>
+        {/* <NavLink
+          to="/profile"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          Profile
+        </NavLink>
+        <NavLink
+          to="/experience"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          Experience
+        </NavLink> */}
+        {sessionStorage.getItem("token") && (
+          <span onClick={handleLogout}>Logout</span>
+        )}
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Navigation() {
   return (
     <div className={styles.navigation}>
-      <Link className={styles["nav-item"]} to="/profile">Profile</Link>
-      <Link className={styles["nav-item"]} to="/experience">Experience</Link>
+      {/* <Link className={styles["nav-item"]} to="/profile">Profile</Link>
+      <Link className={styles["nav-item"]} to="/experience">Experience</Link> */}
+      <Link className={styles["nav-item"]} to="/store">Store</Link>
     </div>
   )
 }
